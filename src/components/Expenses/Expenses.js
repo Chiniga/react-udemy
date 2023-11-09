40ba92dc97a55b451b7f053609ad6e7d3e5729df
David <david.graves@30kft.com>
2023-11-09 16:50:22 +0800

    Properly display expense list content

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,11 +4,16 @@ import './Expenses.css'
 
 const Expenses = props => {
     const filteredExpenses = props?.expenseItems?.filter(exp => exp.date.getFullYear() === +props.filteredYear)
+    let expContent = <p>No Expenses Found</p>
+
+    if(filteredExpenses.length > 0) {
+        expContent = filteredExpenses.map(expense =>
+            <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} filteredYear={props.filteredYear} />
+        )
+    }
     return (
         <Card className='expenses'>
-            {filteredExpenses?.map(expense =>
-                <ExpenseItem key={expense.id} title={expense.title} date={expense.date} amount={expense.amount} filteredYear={props.filteredYear} />
-            )}
+            {expContent}
         </Card>
     )
 }
